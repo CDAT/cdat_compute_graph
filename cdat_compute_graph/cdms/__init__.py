7018e3f601f5721a0128872fb81558b52fa20469
Sam Fries <fries2@llnl.gov>
2017-03-20 17:08:42 -0700

    Compute graph initial commit

--- cdat_compute_graph/cdms/__init__.py
+++ cdat_compute_graph/cdms/__init__.py
@@ -1,4 +1,8 @@
-from arrays import NDArrayBinaryFunction, NDArrayUnaryFunction, NDArrayAxisFunction, NDArraySubsetFunction
-from geospatial import GeospatialFunction
-from dataset import DatasetFunction
-from metadata import MetadataFunction
+try:
+    import cdms2
+    from arrays import NDArrayBinaryFunction, NDArrayUnaryFunction, NDArrayAxisFunction, NDArraySubsetFunction
+    from geospatial import GeospatialFunction
+    from dataset import DatasetFunction
+    from metadata import MetadataFunction
+except:
+    pass
